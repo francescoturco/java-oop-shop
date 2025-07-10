484b3d1346ab1cc0573856d89a6d824d872f95f3
fratur <francescoturco2795@gmail.com>
2025-07-10 19:15:54 +0200

    Attributi-classe-costruttore

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,3 +1,21 @@
+import java.util.Random;
+
 public class Prodotto {
+    
+    
+    //Attribbuti della classe Prodotoo
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public double prezzo;
+    public int iva;
+
+    //Costruttore 
+    public Prodotto(String nome, String descrizione, double prezzo, int iva) {
+        Random rand = new Random();
+        this.codice = rand.nextInt(10000);
+    }
+
+
 
 }
